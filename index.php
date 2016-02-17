9a946f9f8bed99b094e08174affbaf7953e20d05
slowfarm <Altair08111994@yandex.ru>
2016-02-17 11:55:38 +0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+ <title>Пример использования PHP</title>
+ </head>
+
+<body>
+
+<?php
+echo "<html><head>"; 
+echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'; 
+echo "<title>Мой первый PHP скрипт</title>"; 
+echo "</head>"; 
+echo "<body>"; 
+echo "<h1>Привет, Мир!</h1>"; 
+echo "</body></html>"; 
+?>
+
+</body>
+</html>
