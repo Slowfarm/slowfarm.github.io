cf6d5aa7f756147db839dd9b9afc3b1e162123a7
slowfarm <Altair08111994@yandex.ru>
2016-02-28 15:01:41 +0300

    Update vkWallParser.js

--- vkWallParser.js
+++ vkWallParser.js
@@ -8,10 +8,10 @@ function query() {
 	VK.Api.call('wall.get', {owner_id: id, v: '5.27', count: 1000}, function(r) {
 		if(r.response) {
 			$('#resultCount').html(r.response.count);
-				$('#result').html('');
+				$('#profiles').html('');
 				for (var i=0; i<r.response.count; i++)
 				{
-					$('#result').append(''
+					$('#profiles').append(''
 								+ '<li class="c-list user' + r.response.items[i].id + ' pulse animated">'
 								+ '<div class="contact-details">'
 								+ '<div class="pull-left">'
