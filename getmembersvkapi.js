7f56d0b2da9655c6221792486018dd8969c7ee69
slowfarm <Altair08111994@yandex.ru>
2016-02-28 16:37:56 +0300

    Update getmembersvkapi.js

--- getmembersvkapi.js
+++ getmembersvkapi.js
@@ -1,18 +1,16 @@
 VK.init({
     apiId: 5322127
 });
-var membersGroups = []; 
 var user_id = "http://vk.com/miet.university";
 if (user_id.indexOf("com/") >= 0)
 	user_id = user_id.split('com/')[1];
-
-var membersGroups = []; // массив участников группы
 getMembers(20629724);
 
 // получаем информацию о группе и её участников
 function getMembers(group_id) {
 	VK.Api.call('groups.getById', {group_id: group_id, fields: 'photo_50,members_count', v: '5.27'}, function(r) {
 			if(r.response) {
+				id = '-' + r.response[0].id;
 				$('.group_info')
 				.html('<img src="' + r.response[0].photo_50 + '"/><br/>' 
 					+ r.response[0].name
