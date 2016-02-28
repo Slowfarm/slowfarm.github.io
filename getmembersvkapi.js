32ed801f5c853d210765d19f81e044d95962c983
slowfarm <Altair08111994@yandex.ru>
2016-02-28 16:28:33 +0300

    Update getmembersvkapi.js

--- getmembersvkapi.js
+++ getmembersvkapi.js
@@ -17,34 +17,22 @@ function getMembers(group_id) {
 				.html('<img src="' + r.response[0].photo_50 + '"/><br/>' 
 					+ r.response[0].name
 					+ '<br/>Участников: ' + r.response[0].members_count);
-				getMembers20k(group_id, r.response[0].members_count); // получем участников группы и пишем в массив membersGroups
+				getMembers20k();
 			}
 	});
 }
 
 // получаем участников группы, members_count - количество участников
-function getMembers20k(group_id, members_count) {
-	var code =  'var members = API.wall.get({"group_id": ' + group_id + ', "v": "5.27", "count": "100", "offset": ' + membersGroups.length + '}).items;' // делаем первый запрос и создаем массив
-			+	'var offset = 100;' // это сдвиг по участникам группы
-			+	'while (offset < 2500 && (offset + ' + membersGroups.length + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
-			+	'{'
-				+	'members = members + "," + API.wall.get({"group_id": ' + group_id + ', "v": "5.27", "count": "100", "offset": (' + membersGroups.length + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
-				+	'offset = offset + 100;' // увеличиваем сдвиг на 1000
-			+	'};'
-			+	'return members;'; // вернуть массив members
-	alert("до execute");
-	VK.Api.call("execute", {code: code}, function(data) {
-		if (data.response) {
-			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
-			$('.members_ids').html('');
-			$('.member_ids').append('Загрузка: ' + membersGroups.length + '/' + members_count);
-			if (members_count >  membersGroups.length) // если еще не всех участников получили
-				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
-			else // если конец то
-				alert('Ура тест закончен! В массиве membersGroups теперь ' + membersGroups.length + ' элементов.');
-		} else {
-			alert(data.error.error_msg); // в случае ошибки выведем её
-		}
-		alert("во время");
-	});
+function getMembers20k() {
+var code;
+code = 'return {';
+code += 'data: API.wall.get({owner_id: OWNER, offset: 0, count: 15, filter: all, v: "5.37"})';
+code += '};';
+// сам метод execute, выполняет созданный код
+VK.Api.call('execute', {code: code, v: "5.37"}, function(r){
+  if (r.response){
+      if (r.response.data){
+          $('.member_ids').append(r.response.data);
+      }
+  }
 }
