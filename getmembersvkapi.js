1d6e76f63956da7ce55e328a682279eff5b96594
slowfarm <Altair08111994@yandex.ru>
2016-03-16 17:20:31 +0300

    Update getmembersvkapi.js

--- getmembersvkapi.js
+++ getmembersvkapi.js
@@ -20,18 +20,18 @@ function getMembers(group_id) {
 
 // получаем участников группы, members_count - количество участников
 function getMembers20k(group_id, members_count) {
-	var code =  'var members = API.groups.getMembers({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": ' + membersGroups.length + '}).items;' // делаем первый запрос и создаем массив
+	var code =  'var members = API.groups.getMembers({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": ' + membersGroups.length + '});' // делаем первый запрос и создаем массив
 			+	'var offset = 100;' // это сдвиг по участникам группы
 			+	'while (offset < 2500 && (offset + ' + membersGroups.length + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
 			+	'{'
-				+	'members = members + "," + API.groups.getMembers({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": (' + membersGroups.length + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
+				+	'members = members + "," + API.groups.getMembers({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": (' + membersGroups.length + ' + offset)});' // сдвиг участников на offset + мощность массива
 				+	'offset = offset + 100;' // увеличиваем сдвиг на 1000
 			+	'};'
 			+	'return members;'; // вернуть массив members
 	
 	VK.Api.call("execute", {code: code}, function(data) {
 		if (data.response) {
-			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
+			membersGroups = membersGroups.concat(JSON.parse("[" + data.response.items + "]")); // запишем это в массив
 			$('.member_ids').html('Загрузка: ' + membersGroups.length + '/' + members_count);
 			if (members_count >  membersGroups.length) // если еще не всех участников получили
 				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
