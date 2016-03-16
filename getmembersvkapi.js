bc26f8fc3ef047e3c40a0603de189ce31f8eabf5
slowfarm <Altair08111994@yandex.ru>
2016-03-16 17:30:11 +0300

    Update getmembersvkapi.js

--- getmembersvkapi.js
+++ getmembersvkapi.js
@@ -32,7 +32,8 @@ function getMembers20k(group_id, members_count) {
 	VK.Api.call("execute", {code: code}, function(data) {
 		if (data.response) {
 			membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
-			$('.member_ids').html('Загрузка: ' + membersGroups.length + '/' + members_count);
+			for (var i=0; i< 25000; i++)
+				$('.member_ids').html('Загрузка: ' + membersGroups[i] + '/' + members_count);
 			if (members_count >  membersGroups.length) // если еще не всех участников получили
 				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
 			else // если конец то
