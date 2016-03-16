1a434b34358ffd4f4e1444a9741c5f3f13c222c4
slowfarm <Altair08111994@yandex.ru>
2016-03-16 18:01:18 +0300

    Update getmembersvkapi.js

--- getmembersvkapi.js
+++ getmembersvkapi.js
@@ -3,6 +3,7 @@ VK.init({
 });
 
 var membersGroups = []; 
+var counter = 0;
 getMembers(20629724);
 
 
@@ -20,25 +21,26 @@ function getMembers(group_id) {
 
 // получаем участников группы, members_count - количество участников
 function getMembers20k(group_id, members_count) {
-	var code =  'var members = API.wall.get({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": (' + membersGroups.length + ')}).items;'
+	var code =  'var members = API.wall.get({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": (' + counter + ')}).items;'
 			+	'var offset = 100;' // это сдвиг по участникам группы
-			+	'while (offset < 2500 && (offset + ' + membersGroups.length + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
+			+	'while (offset < 2500 && (offset + ' + counter + ') < ' + members_count + ')' // пока не получили 20000 и не прошлись по всем участникам
 			+	'{'
-				+	'members = members + "," + API.wall.get({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": (' + membersGroups.length + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
+				+	'members = members + "," + API.wall.get({"owner_id": -' + group_id + ', "v": "5.27", "count": "100", "offset": (' + counter + ' + offset)}).items;' // сдвиг участников на offset + мощность массива
 				+	'offset = offset + 100;' // увеличиваем сдвиг на 1000
 			+	'};'
 			+	'return members;'; // вернуть массив members
 	
 	VK.Api.call("execute", {code: code}, function(data) {
 		if (data.response) {
-			for(var i=0; i< 100; i++)
-				membersGroups = membersGroups.concat(JSON.parse("[" + data.response[i].text + "]")); // запишем это в массив
-			for (var i=0; i< 2500; i++)
-				$('.member_ids').html('Загрузка: ' + membersGroups.length + '/' + members_count);
-			if (members_count >  membersGroups.length) // если еще не всех участников получили
+			//for(var i=0; i< 100; i++)
+			//	membersGroups = membersGroups.concat(JSON.parse("[" + data.response + "]")); // запишем это в массив
+			for (var i=0; i< 250; i++)
+				$('.member_ids').append('Загрузка: ' + data.response[i].text + '/' + members_count);
+			if (members_count >  counter) 
+				counter+=2500;
 				setTimeout(function() { getMembers20k(group_id, members_count); }, 333); // задержка 0.333 с. после чего запустим еще раз
 			else // если конец то
-				alert('Ура тест закончен! В массиве membersGroups теперь ' + membersGroups.length + ' элементов.');
+				//alert('Ура тест закончен! В массиве membersGroups теперь ' + membersGroups.length + ' элементов.');
 		} else {
 			alert(data.error.error_msg); // в случае ошибки выведем её
 		}
