f2b22f75900652c6b3effe1a3aec791a5e3acd99
slowfarm <Altair08111994@yandex.ru>
2016-10-07 16:38:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
-<!doctype html>
-<html>
- <head>
-   <title>PHP</title>
- </head>
-<body>
- <script>
- <?php
- $sdd_db_host='178.208.81.185'; // ваш хост
- $sdd_db_name='vova_test'; // ваша бд
- $sdd_db_user='vova_test'; // пользователь бд
- $sdd_db_pass='admin123'; // пароль к бд
- @mysql_connect($sdd_db_host,$sdd_db_user,$sdd_db_pass); // коннект с сервером бд
- @mysql_select_db($sdd_db_name); // выбор бд
- $result=mysql_query('SELECT * FROM `test_table`'); // запрос на выборку
- while($row=mysql_fetch_array($result))
- { 
- echo json_encode($row[text]);// выводим данные
- }
- ?>
- </script>
-</body>
-</html>
+<?php
+$sdd_db_host='localhost'; 
+$sdd_db_name='mysql'; 
+$sdd_db_user='mysql'; 
+$sdd_db_pass='';
+@mysql_connect($sdd_db_host,$sdd_db_user,$sdd_db_pass); 
+@mysql_select_db($sdd_db_name); // выбор бд
+$result=mysql_query('SELECT * FROM `myjson`');
+$str = "[";
+while($row = mysql_fetch_array($result))
+$str = $str.'{"author":"'.$row[author].'","label":"'.$row[label].'"},';
+$str = substr($str,0,-1);
+$str = $str.']';
+echo $str;
+?>
